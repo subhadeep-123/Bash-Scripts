4dc5f0138b63dacd88b2e880f09a2deea6492f7c
Subhadeep Banerjee <subhadeep@klizos.com>
2020-12-21 01:25:14 +0530

    Added a new Something

--- SimpleShellTutorial/04_Logical_Operator_Demos.sh
+++ SimpleShellTutorial/04_Logical_Operator_Demos.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 clear
+
+: '
+eq - equal to
+ne - not equal to
+le - less then or equal to
+lt - less than
+ge - greater that or equal to
+gt - greater that
+'
+
 read -p "What is your name - " name
 echo Hello $name
 
@@ -9,4 +19,16 @@ if [ $age -ge 16 ]; then
     echo You can Drive $name
 else
     echo You are not allowed to drive $name
+fi
+
+read -p "Enter a number : " num
+
+if ((num == 10)); then
+    echo "The val is 10"
+elif ((num >= 10)); then
+    echo "It is less than 10"
+fi
+
+if ((num > 0 && num < 11)); then
+    echo "Num is betweem 0 ~ 10"
 fi
